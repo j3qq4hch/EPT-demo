7954f8a76e184ac587af34bf01b7d517332912c1
j3qq4hch <j3qq4hch@gmail.com>
2026-03-28 14:32:19 +0300

    CFG upda

--- ept_cfg.h
+++ ept_cfg.h
@@ -1,15 +1,7 @@
 #ifndef __EPT_CFG
 #define __EPT_CFG
 
-// This is user-provided file
-
-// These definitions are to be configured for a specific case
-// It is presumed here that ANY system has at least one debug CLI
-//#define CLI_BUFLEN         192
-//#define CLI_MAX_ARGNUM     5
-//#define CLI_SERIAL_PTR     (&SERIAL1)
-////........................................................////
-#define PROFILER                     //Uncomment this if profiler needed
+#define PROFILER         //Uncomment this if profiler needed
 #define LOW_POWER_MODE
 
 #ifdef LOW_POWER_MODE
@@ -17,10 +9,14 @@ extern volatile uint8_t tc_flag;
 #define EPT_TICK() { ept_timer_tick(); tc_flag = 1; }
 #else
 #define EPT_TICK() ept_timer_tick();
-#endif
+#endif //#ifdef LOW_POWER_MODE
+
+#endif //#ifndef __EPT_CFG
 
 #ifdef EPT_SCHEDULER_IMPLEMENTATION
 
+#ifndef __EPT_SCHEDULER_IMPLEMENTATION__
+#define __EPT_SCHEDULER_IMPLEMENTATION__
 //threadlist array must be filled manually by the user 
 //All the top level threads must be placed in this array
 #include "blinker.h"
